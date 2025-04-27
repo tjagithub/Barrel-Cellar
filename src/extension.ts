ff5adb7811c43aad535692b0a00486e61a125f44
TJ Appleby <tjaemails@gmail.com>
2025-04-27 22:01:36 +0100

    feat: Update version to 0.0.2, add new icons, and refactor treeview structure

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { BarrelFilesProvider } from './barrel.treeview';
+import { BarrelFilesProvider } from './treeview';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "debarrel" is now active!');
-	vscode.window.registerTreeDataProvider('barrel-cellar', new BarrelFilesProvider(context));
+	vscode.window.registerTreeDataProvider('barrel.cellar', new BarrelFilesProvider(context));
 }
 
 // This method is called when your extension is deactivated
